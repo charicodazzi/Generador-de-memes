b00b049f97188b375782cdaf280e4478a0966310
charicodazzi <charicodazzi@hotmail.com>
2021-04-27 21:46:10 -0300

    funcionalidades

--- app.js
+++ app.js
@@ -1,54 +1,69 @@
-const panel = document.querySelector("#panel");
+"use strict";
+//LLamo al panel aside y al boton cruz
+
+const panelAside = document.querySelector("#panelAside");
 const buttonClose = document.querySelector("#button-close");
 
-//cuando hago click en boton de cerra se cierra el panel(aside completo)
-//agregar la clase ocular
+//cuando hago click en boton CRUZ se cierra el panel(aside completo)
+//agregar la clase ocultar
 
 buttonClose.addEventListener("click", () => {
-  panel.classList.add("ocultar");
+  panelAside.classList.add("ocultar");
 });
-const panel = document.querySelector("#panel");
-const panelImg = document.querySelector("#panel-img");
+
+const panelImg = document.querySelector("#panel-image");
 const panelText = document.querySelector("#panel-text");
 
-const buttonClose = document.querySelector("#button-close");
 const buttonImg = document.querySelector("#button-img");
 const buttonText = document.querySelector("#button-text");
 
-//cuando hago click en boton de cerra se cierra el panel(aside completo)
-//agregar la clase ocular
-
-buttonClose.addEventListener("click", () => {
-  panel.classList.add("ocultar");
-});
-
-// cuando hago click en el boton de imagen o en el boton texto debe verse el panel
-// quitar la clase ocular
-buttonText.addEventListener("click", () => {
-  panel.classList.remove("ocultar");
-});
-
+// --------VER PANEL IMAGEN--------
 buttonImg.addEventListener("click", () => {
-  panel.classList.remove("ocultar");
-});
-const panel = document.querySelector("#panel");
-const panelImg = document.querySelector("#panel-img");
-const panelText = document.querySelector("#panel-text");
-
-const buttonClose = document.querySelector("#button-close");
-const buttonImg = document.querySelector("#button-img");
-const buttonText = document.querySelector("#button-text");
-buttonClose.addEventListener("click", () => {
-  panel.classList.add("ocultar");
-});
-buttonText.addEventListener("click", () => {
-  panel.classList.remove("ocultar");
-  panelImg.classList.add("ocultar");
-  panelText.classList.remove("ocultar");
-});
-
-buttonImg.addEventListener("click", () => {
-  panel.classList.remove("ocultar");
+  panelAside.classList.remove("ocultar");
   panelText.classList.add("ocultar");
   panelImg.classList.remove("ocultar");
 });
+
+// --------VER PANEL TEXTO--------
+buttonText.addEventListener("click", () => {
+  panelAside.classList.remove("ocultar");
+  panelText.classList.remove("ocultar");
+  panelImg.classList.add("ocultar");
+});
+
+//--------MODO CLARO/OSCURO--------------//
+const body = document.querySelector("body");
+const claroOscuro = document.querySelector("#claroOscuro");
+
+claroOscuro.addEventListener("click", () => {
+  body.classList.toggle("modoClaro");
+  body.classList.toggle("oscuro");
+
+  if (claroOscuro.innerText == "Modo Claro") {
+    claroOscuro.innerText = "Modo Oscuro";
+  } else {
+    claroOscuro.innerText = "Modo Claro";
+  }
+});
+
+//----Modificacion texto superior/inferior----//
+const topTitleMeme = document.querySelector(".memeTextTop");
+const topText = document.getElementById("topText");
+
+topText.addEventListener("input", () => {
+  topTitleMeme.textContent = topText.value;
+});
+
+const bottomTitleMeme = document.querySelector(".memeTextBottom");
+const bottomText = document.getElementById("bottomText");
+
+bottomText.addEventListener("input", () => {
+  bottomTitleMeme.textContent = bottomText.value;
+});
+//OCULTAR TOP TEXT
+/*const textoSuperior = document.getElementById("textoSuperior");
+const ocultarTextoSuperior = document.querySelector(".header-generator");
+
+textoSuperior.addEventListener("click", () =>{
+    ocultarTextoSuperior.classList.toggle("ocultar-panel");
+});*/
